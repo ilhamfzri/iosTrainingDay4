736d861d5791c4f8c888af0371a5a50cea905c12
Ilham Fazri <ilham_fazri@bca.co.id>
2023-10-19 09:32:19 +0700

    Initial Commit

---
+++ iosTrainingDay4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosTrainingDay4
+//
+//  Created by ITBCA on 19/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
